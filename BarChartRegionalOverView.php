74a7f31cc61ecec73afb8ab6c1aa6a0fbff921d3
sanketpurandare <sanketpurandare@gmail.com>
2015-07-12 03:41:15 +0530

    lol

--- BarChartRegionalOverView.php
+++ BarChartRegionalOverView.php
@@ -28,8 +28,8 @@ if ($conn->connect_error) {
 } 
 echo "Connected successfully";
 
-$sql="select * from economic_impact_region;";
-$sql2="select * from economic_impact_hhf ;";
+$sql="select * from regional_overview;";
+$sql2="select * from regional_overview_hhf ;";
 
 $result = $conn->query($sql);
 
@@ -37,11 +37,11 @@ if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
        // echo "child " . $row["cost_child"]. " employment : " . $row["employment_increase"]. " " . $row["employment_increase"]. "<br>";
-        $cost_child=$row["cost_child"];
-        $employment_increase=$row["employment_increase"];
-        $income_increase=$row["income_increase"];
-        $shelter_increase=$row["shelter_increase"];
-        $volunteer_hours=$row["volunteer_hours"];
+        $attendance_rate=$row["attendence_rate"];
+        $computer_literacy=$row["computer_literacy"];
+        $language_literacy=$row["language_literacy"];
+        $retention_rate=$row["retention_rate"];
+        $tertiary=$row["tertiary"];
     }
 } else {
     echo "0 results";
@@ -53,12 +53,12 @@ $result2 = $conn->query($sql2);
 if ($result2->num_rows > 0) {
     // output data of each row
     while($row2 = $result2->fetch_assoc()) {
-      //  echo "child " . $row2["cost_child"]. " employment : " . $row2["employment_increase"]. " " . $row2["employment_increase"]. "<br>";
-        $cost_child2=$row2["cost_child"];
-        $employment_increase2=$row2["employment_increase"];
-        $income_increase2=$row2["income_increase"];
-        $shelter_increase2=$row2["shelter_increase"];
-        $volunteer_hours2=$row2["volunteer_hours"];
+       // echo "child " . $row2["attendence_rate"]. " employment : " . $row2["computer_literacy"]. " " . $row2["language_literacy"]. "<br>";
+        $attendance_rate2=$row2["attendence_rate"];
+        $computer_literacy2=$row2["computer_literacy"];
+        $language_literacy2=$row2["language_literacy"];
+        $retention_rate2=$row2["retention_rate"];
+        $tertiary2=$row2["tertiary"];
     }
 } else {
     echo "0 results";
@@ -70,7 +70,7 @@ $conn->close();
     <script lang="javascript" type="text/javascript">
         $(document).ready(function () {
             $('#jqChart').jqChart({
-                title: { text: 'Bar Chart Comparison for Economic Impacts: ' },
+                title: { text: 'Bar Chart Comparison for Regional Overview: ' },
                 animation: { duration: 1 },
                 shadows: {
                     enabled: true
@@ -80,15 +80,15 @@ $conn->close();
                         type: 'bar',
                         title: 'Regional',
                         fillStyle: '#418CF0',
-                        data: [['Increase in Average household Income of Students Family' , <?php echo $income_increase?>], ['Increase in Villagers Employment', <?php echo $employment_increase?>], ['Number of Villagers Sheltering in HHF School', <?php echo $shelter_increase?>],
-                               ['Cost per child', <?php echo $cost_child ?>], ['Community_volunteers_hours', <?php echo $volunteer_hours?>]]
+                        data: [['Attendance Rate' , <?php echo $attendance_rate?>], ['Computer Literacy', <?php echo $computer_literacy?>], ['Language Literacy', <?php echo $language_literacy?>],
+                               ['Students Attending Tertiary College', <?php echo $tertiary ?>], ['Retention Rate', <?php echo $retention_rate?>]]
                     },
                     {
                         type: 'bar',
                         title: 'Happy Hearts Fund',
                         fillStyle: '#FCB441',
-                        data: [['Increase in Average household Income of Students Family', <?php echo $income_increase2?>], ['Increase in Villagers Employment', <?php echo $employment_increase2?>], ['Number of Villagers Sheltering in HHF School', <?php echo $shelter_increase2?>],
-                               ['Cost per child', <?php echo $cost_child2?>], ['Community_volunteers_hours', <?php echo $volunteer_hours2?>]]
+                        data: [['Attendance Rate' , <?php echo $attendance_rate2 ?>], ['Computer Literacy', <?php echo $computer_literacy2 ?>], ['Language Literacy', <?php echo $language_literacy2 ?>],
+                               ['Students Attending Tertiary College', <?php echo $tertiary2 ?>], ['Retention Rate', <?php echo $retention_rate2 ?>]]
                     }
                 ]
             });
